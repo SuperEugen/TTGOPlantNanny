2d3017ba50e230729dad3cbda1fbe95a64be66ef
Ingo Hoffmann <IngoEHoffmann@gmail.com>
2022-06-22 13:53:07 +0200

    secrets
    enter your own values

---
+++ secrets.h
@@ -0,0 +1,16 @@
+//***************************************************************************************************
+//  secrets:      Exclude this from GitHub
+//***************************************************************************************************
+
+#ifndef secrets_h
+#define secrets_h
+
+#define SECRET_WIFI_SSID      ""
+#define SECRET_WIFI_PASSWORD  ""
+#define SECRET_MQTT_BROKER    ""
+#define SECRET_MQTT_PORT      1883
+#define SECRET_MQTT_USER      ""
+#define SECRET_MQTT_PASSWORD  ""
+String SECRET_OWM_APP_ID =    "";
+
+#endif
