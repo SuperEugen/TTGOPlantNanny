23542563352f77e44de95f1cd800bfa50abdff6a
Ingo Hoffmann <IngoEHoffmann@gmail.com>
2022-06-23 15:09:23 +0200

    secrets test

---
+++ secrets-dummy.h
@@ -0,0 +1,15 @@
+//***************************************************************************************************
+//  secrets:      enter your values and rename this file to secrets.h
+//***************************************************************************************************
+
+#ifndef secrets_h
+#define secrets_h
+
+#define SECRET_WIFI_SSID      "your SSID"
+#define SECRET_WIFI_PASSWORD  "your wifi password"
+#define SECRET_MQTT_BROKER    "your mqtt server"
+#define SECRET_MQTT_PORT      1883
+#define SECRET_MQTT_USER      "your mqtt user name"
+#define SECRET_MQTT_PASSWORD  "your mqtt user password"
+
+#endif
